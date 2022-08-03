59ca9d91e4fb6379048989a79d4d5b6c63e771d4
xqy2006 <2601928946@qq.com>
2022-08-03 22:35:52 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { useDynamicPublicPath } from 'vite-plugin-dynamic-publicpath'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),useDynamicPublicPath('/primer-blog/'),],
+  plugins: [vue()],
+  base: './'
 })
